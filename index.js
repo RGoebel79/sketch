8d0cf14947592c483ab6277886ac2c369706cb0a
RonG <racg550@gmail.com>
2016-10-17 13:37:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+ $(document).ready(function() {
+
+   var pixels = 0;
+   var row;
+   var col;
+   var i = 0;
+   var j = 0;
+   var ele = document.getElementsByClassName("column");
+   var modal = document.getElementById("modal");
+
+   function done() {
+     $(".column").mouseenter(function() {
+
+       $(this).fadeTo(150, 0);
+     });
+   }
+
+ var grid = function(pix) {
+
+     var high = (960 / pixels) - 2;
+     var wide = (960 / pixels) - 3;
+
+     for (i = 0; i <= pix; i++) {
+       row = '<div id=\"row' + i + '\" class=\"row\"></div>';
+       $("#container").append(row);
+       for (j = 0; j <= pix; j++) {
+         col = '<div id=\"col' + j + '\" class=\"column\"></div>';
+         $("#row" + i).append(col);
+
+       }
+     }
+     $(".column").height(high);
+     $(".column").width(wide);
+     $(".row").height(high + 2);
+     done();
+
+   }
+
+   $(".sub").click(function(event) {
+     event.preventDefault();
+     pixels = document.getElementById("pixels").value;
+     var final = pixels - 1;
+     modal.style.display = "none";
+
+     grid(final);
+   });
+
+   document.getElementById("reset").addEventListener("click", function() {
+
+     $(".column").remove();
+     $(".row").remove();
+     modal.style.display = "block";
+
+   });
+
+ });
